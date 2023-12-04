83c776738c7f3e39515d9accb24b26f322359973
D3ENNY <denysraimondi06@gmail.com>
2023-12-04 23:35:54 +0100

    fuck day 3 challenge 1

--- 2023/day3/challenge1/src/App.java
+++ 2023/day3/challenge1/src/App.java
@@ -1,7 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.IntStream;
@@ -12,110 +11,162 @@ public class App {
     private Scanner sc;
     private List<String> contentFile;
 
-    public App(){
-        try{
+    public App() {
+        try {
             this.sc = new Scanner(new File("src/input.txt"));
             this.contentFile = new ArrayList<>();
-        }catch(FileNotFoundException e){
+        } catch (FileNotFoundException e) {
             System.err.println(e.getMessage());
         }
     }
 
     private void challenge1() {
         getFullList();
-        AtomicInteger result = new AtomicInteger(0);
-    
-        IntStream.range(0, contentFile.size())
-            .forEach(index -> {
-                String x = contentFile.get(index);
-                List<String> numbers = findMatches(x, "\\d+");
-    
-                int[] nIndex = getAllIndex(numbers, x);
-    
-                String checkStr = "";
-                if (index != 0 && index != contentFile.size() - 1) {
-                    for (int j = 0; j < nIndex.length; j++) {
-                        checkStr="";
-                        int nLenght = numbers.get(j).toCharArray().length;
-                       
-                        if (nIndex[j] != 0 && nIndex[j] != x.length()-1 && nIndex[j] <= x.length() - (nLenght + 1)) {
-                            checkStr = contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) +
-                                    x.substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) +
-                                    contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1);
-                                    
-                        } else if (nIndex[j] == 0) {
-                            checkStr = contentFile.get(index - 1).substring(nIndex[j], nIndex[j] + nLenght + 1) +
-                                    x.substring(nIndex[j], nIndex[j] + nLenght + 1) +
-                                    contentFile.get(index + 1).substring(nIndex[j], nIndex[j] + nLenght + 1);
-                                    
-                        }else if(nIndex[j] == x.length()-1){
-                            checkStr = contentFile.get(index - 1).substring(nIndex[j]-1, nIndex[j]+1) +
-                                    x.substring(nIndex[j]-1, nIndex[j]+1) +
-                                    contentFile.get(index + 1).substring(nIndex[j]-1, nIndex[j] + 1);
+        int result = 0;
 
-                        } else if (nIndex[j] > x.length() - (nLenght + 1)) {
-                            checkStr = contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght) +
-                                    x.substring(nIndex[j] - 1, nIndex[j] + nLenght) +
-                                    contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght);
+        int[] tmp = IntStream.range(0, contentFile.size()).toArray();
+        for (int index : tmp) {
+            String x = contentFile.get(index);
+            List<String> numbers = findMatches(x, "\\d+");
 
-                        }
-                        if (isMatch(checkStr, "^(?![.\\d]+$).+$")) {
-                            result.addAndGet(Integer.parseInt(numbers.get(j)));
-                        }
+            int[] nIndex = getAllIndex(numbers, x);
+
+            String checkStr = "";
+            if (index != 0 && index != contentFile.size() - 1) {
+                for (int j = 0; j < nIndex.length; j++) {
+                    int nLenght = numbers.get(j).toCharArray().length;
+
+                    if (nIndex[j] != 0 && nIndex[j] != x.length() - 1 && nIndex[j] <= x.length() - (nLenght + 1)) {
+                        checkStr = contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) +
+                                x.substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) +
+                                contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1);
+
+                        System.out.println(
+                                contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) + "\n" +
+                                        x.substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) + "\n" +
+                                        contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1)
+                                        + "\n*\n\n");
+
+                    } else if (nIndex[j] == 0) {
+                        checkStr = contentFile.get(index - 1).substring(nIndex[j], nIndex[j] + nLenght + 1) +
+                                x.substring(nIndex[j], nIndex[j] + nLenght + 1) +
+                                contentFile.get(index + 1).substring(nIndex[j], nIndex[j] + nLenght + 1);
+
+                        System.out.println(contentFile.get(index - 1).substring(nIndex[j], nIndex[j] + nLenght + 1)
+                                + "\n" +
+                                x.substring(nIndex[j], nIndex[j] + nLenght + 1) + "\n" +
+                                contentFile.get(index + 1).substring(nIndex[j], nIndex[j] + nLenght + 1) + "\n**\n\n");
+
+                    } else if (nIndex[j] == x.length() - 1) {
+                        checkStr = contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + 1) +
+                                x.substring(nIndex[j] - 1, nIndex[j] + 1) +
+                                contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + 1);
+
+                        System.out.println(contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + 1) + "\n" +
+                                x.substring(nIndex[j] - 1, nIndex[j] + 1) + "\n" +
+                                contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + 1) + "\n***\n\n");
+
+                    } else if (nIndex[j] > x.length() - (nLenght + 1)) {
+                        checkStr = contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght) +
+                                x.substring(nIndex[j] - 1, nIndex[j] + nLenght) +
+                                contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght);
+
+                        System.out.println(
+                                contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght) + "\n" +
+                                        x.substring(nIndex[j] - 1, nIndex[j] + nLenght) + "\n" +
+                                        contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght)
+                                        + "\n****\n\n");
+
                     }
-                } else if (index == 0) {
-                    for (int j = 0; j < nIndex.length; j++) {
-                        checkStr="";
-                        int nLenght = numbers.get(j).toCharArray().length;
-                        if (nIndex[j] != 0 && nIndex[j] != x.length() && nIndex[j] <= x.length() - (nLenght + 1)) {
-                            checkStr = x.substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) +
-                                    contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1);
-
-                        } else if (nIndex[j] == 0) {
-                            checkStr = x.substring(nIndex[j], nIndex[j] + nLenght + 1) +
-                                    contentFile.get(index + 1).substring(nIndex[j], nIndex[j] + nLenght + 1);
-
-                        }else if(nIndex[j] == x.length()-1){
-                            checkStr = x.substring(nIndex[j]-1, nIndex[j]+1) +
-                                    contentFile.get(index + 1).substring(nIndex[j]-1, nIndex[j]+1);
-
-                        }else if (nIndex[j] > x.length() - (nLenght + 1)) {
-                            checkStr = x.substring(nIndex[j] - 1, nIndex[j] + nLenght) +
-                                    contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght);
-
-                        }
-                        if (isMatch(checkStr, "^(?![.\\d]+$).+$")) {
-                            result.addAndGet(Integer.parseInt(numbers.get(j)));
-                        }
-                    }
-                } else if (index == contentFile.size() - 1) {
-                    for (int j = 0; j < nIndex.length; j++) {
-                        checkStr="";
-                        int nLenght = numbers.get(j).toCharArray().length;
-                        if (nIndex[j] != 0 && nIndex[j] != x.length() && nIndex[j] <= x.length() - (nLenght + 1)) {
-                            checkStr = contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) +
-                                    x.substring(nIndex[j] - 1, nIndex[j] + nLenght + 1);
-                        } else if (nIndex[j] == 0) {
-                            checkStr = contentFile.get(index - 1).substring(nIndex[j], nIndex[j] + nLenght + 1) +
-                                    x.substring(nIndex[j], nIndex[j] + nLenght + 1);
-                        }else if(nIndex[j] == x.length()-1){
-                            checkStr = x.substring(nIndex[j]-1, nIndex[j]) +
-                                    contentFile.get(index -1).substring(nIndex[j]-1, nIndex[j]);
-                            System.out.println(checkStr);
-                        } else if (nIndex[j] > x.length() - (nLenght + 1)) {
-                            checkStr = contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght) +
-                                    x.substring(nIndex[j] - 1, nIndex[j] + nLenght);
-                        }
-                        if (isMatch(checkStr, "^(?![.\\d]+$).+$")) {
-                            result.addAndGet(Integer.parseInt(numbers.get(j)));
-                        }
+                    if (isMatch(checkStr, "^(?![.\\d]+$).+$")) {
+                        result += Integer.parseInt(numbers.get(j));
                     }
                 }
-            });
-        System.out.println(result.get());
+            } else if (index == 0) {
+                for (int j = 0; j < nIndex.length; j++) {
+                    int nLenght = numbers.get(j).toCharArray().length;
+                    if (nIndex[j] != 0 && nIndex[j] != x.length() && nIndex[j] <= x.length() - (nLenght + 1)) {
+                        checkStr = x.substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) +
+                                contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1);
+
+                        System.out.println(x.substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) + "\n" +
+                                contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1)
+                                + "\n-\n\n");
+
+                    } else if (nIndex[j] == 0) {
+                        checkStr = x.substring(nIndex[j], nIndex[j] + nLenght + 1) +
+                                contentFile.get(index + 1).substring(nIndex[j], nIndex[j] + nLenght + 1);
+
+                        System.out.println(x.substring(nIndex[j], nIndex[j] + nLenght + 1) + "\n" +
+                                contentFile.get(index + 1).substring(nIndex[j], nIndex[j] + nLenght + 1) + "\n--\n\n");
+
+                    } else if (nIndex[j] == x.length() - 1) {
+                        checkStr = x.substring(nIndex[j] - 1, nIndex[j] + 1) +
+                                contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + 1);
+
+                        System.out.println(x.substring(nIndex[j] - 1, nIndex[j] + 1) + "\n" +
+                                contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + 1) + "\n---\n\n");
+
+                    } else if (nIndex[j] > x.length() - (nLenght + 1)) {
+                        checkStr = x.substring(nIndex[j] - 1, nIndex[j] + nLenght) +
+                                contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght);
+
+                        System.out.println(x.substring(nIndex[j] - 1, nIndex[j] + nLenght) + "\n" +
+                                contentFile.get(index + 1).substring(nIndex[j] - 1, nIndex[j] + nLenght)
+                                + "\n----\n\n");
+
+                    }
+                    if (isMatch(checkStr, "^(?![.\\d]+$).+$")) {
+                        result += Integer.parseInt(numbers.get(j));
+                    }
+
+                }
+            } else if (index == contentFile.size() - 1) {
+                for (int j = 0; j < nIndex.length; j++) {
+                    int nLenght = numbers.get(j).toCharArray().length;
+                    if (nIndex[j] != 0 && nIndex[j] != x.length() && nIndex[j] <= x.length() - (nLenght + 1)) {
+                        checkStr = contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) +
+                                x.substring(nIndex[j] - 1, nIndex[j] + nLenght + 1);
+
+                        System.out.println(
+                                contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) + "\n" +
+                                        x.substring(nIndex[j] - 1, nIndex[j] + nLenght + 1) + "\n,\n\n");
+
+                    } else if (nIndex[j] == 0) {
+                        checkStr = contentFile.get(index - 1).substring(nIndex[j], nIndex[j] + nLenght + 1) +
+                                x.substring(nIndex[j], nIndex[j] + nLenght + 1);
+
+                        System.out.println(
+                                contentFile.get(index - 1).substring(nIndex[j], nIndex[j] + nLenght + 1) + "\n" +
+                                        x.substring(nIndex[j], nIndex[j] + nLenght + 1) + "\n,,\n\n");
+
+                    } else if (nIndex[j] == x.length() - 1) {
+                        checkStr = x.substring(nIndex[j] - 1, nIndex[j]) +
+                                contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j]);
+
+                        System.out.println(x.substring(nIndex[j] - 1, nIndex[j]) + "\n" +
+                                contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j]) + "\n,,,\n\n");
+
+                    } else if (nIndex[j] > x.length() - (nLenght + 1)) {
+                        checkStr = contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght) +
+                                x.substring(nIndex[j] - 1, nIndex[j] + nLenght);
+
+                        System.out.println(
+                                contentFile.get(index - 1).substring(nIndex[j] - 1, nIndex[j] + nLenght) + "\n" +
+                                        x.substring(nIndex[j] - 1, nIndex[j] + nLenght) + "\n,,,,\n\n");
+
+                    }
+                    if (isMatch(checkStr, "^(?![.\\d]+$).+$")) {
+                        result += Integer.parseInt(numbers.get(j));
+                    }
+                }
+            }
+            // new Scanner(System.in).nextLine();
+        }
+        System.out.println(result);
     }
 
-    private int[] getAllIndex(List<String> numbers, String x){
+    private int[] getAllIndex(List<String> numbers, String x) {
         List<Integer> nIndex = new ArrayList<>();
         int lastIndex = 0;
         for (String n : numbers) {
@@ -135,9 +186,9 @@ public class App {
         List<String> returnList = new ArrayList<>();
         Matcher matcher = Pattern.compile(regex).matcher(inputStr);
 
-        while(matcher.find())
+        while (matcher.find())
             returnList.add(matcher.group());
-        
+
         return returnList;
     }
 
